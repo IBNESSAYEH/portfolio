54aa0686b20d7050ad98b52e129bb0a97b52821d
IBNESSAYEH <abdellatifibnessayeh@gmail.com>
2023-11-20 23:53:13 +0100

    make js event

--- script/script.js
+++ script/script.js
@@ -1,12 +1,24 @@
 
-let navIcon =document.querySelector('.material-symbols-outlined')
+let navIcon =document.querySelector('.nav-icon1')
 
 
   navIcon.addEventListener('click',function(){
     var navSm = document.getElementsByClassName("nav-sm");
     navSm = Array.from(navSm)
     navSm.forEach(element => {
-        element.style.display=== "none" ? element.style.display= "inline-block" : element.style.display= "none"
-        element.style.transform= "rotate(360deg)"
+        element.style.display=== "none" ? element.style.display= "inline-block" : element.style.display= "none";
+        
     });
   })
+
+
+
+  document.querySelector('.toggle-switch').addEventListener('click', function(event) { 
+    event.preventDefault();
+    const themeStyle = document.getElementById('theme-style'); 
+    if (themeStyle.getAttribute('href') === 'style/index_light_mode.css') {
+       themeStyle.setAttribute('href', 'style/index_dark_mode.css'); 
+      } else {
+         themeStyle.setAttribute('href', 'style/index_light_mode.css'); 
+        } 
+      });
